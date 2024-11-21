f4627808c1fe11c283fb0f9ccadb486e78689ea5
SHRESTH SRIVASTAV <shresthsrivastav01@gmail.com>
2024-11-21 12:23:34 +0530

    Add files via upload

---
+++ HandTrackingModule.py
@@ -0,0 +1,67 @@
+import cv2
+import mediapipe as mp
+import time
+
+class HandDetector():
+    def __init__(self, mode=False, maxHands=2, detectionCon=0.5, trackCon=0.5):
+        self.mode = mode
+        self.maxHand = maxHands
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(static_image_mode=self.mode, 
+                                        max_num_hands=self.maxHand,
+                                        min_detection_confidence=self.detectionCon,
+                                        min_tracking_confidence=self.trackCon)
+        self.mpDraw = mp.solutions.drawing_utils
+
+    def findHands(self, img, draw=True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB)
+
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
+
+        return img
+
+    def findposition(self, img, handNo=0, draw=True):
+        lmList = []
+        if self.results.multi_hand_landmarks:  # Check if results are available
+            if len(self.results.multi_hand_landmarks) > handNo:  # Ensure handNo is valid
+                myHand = self.results.multi_hand_landmarks[handNo]
+                for id, lm in enumerate(myHand.landmark):
+                    h, w, c = img.shape
+                    cx, cy = int(lm.x * w), int(lm.y * h)
+                    lmList.append([id, cx, cy])
+                    if draw:
+                        cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+        return lmList
+
+def main():
+    pTime = 0
+    cTime = 0
+    cap = cv2.VideoCapture(0)
+    detector = HandDetector()
+    while True:
+        success, img = cap.read()
+        if not success:
+            break
+        img = detector.findHands(img)
+        lmList = detector.findposition(img)
+        if len(lmList) != 0:
+            print(lmList[4])  # Prints the landmark for the tip of the thumb
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3,
+                    (255, 0, 255), 3)
+
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
+
+
+if __name__ == "__main__":
+    main()
